ecca13c79794df7db0f70194baa33d3d4528c78b
Georgina Appeagyei <georginaappeagyei@Georginas-MBP.lan>
2023-06-29 15:00:40 +0100

    new task test

--- tests/add_new_task_test.js
+++ tests/add_new_task_test.js
@@ -1,17 +1,9 @@
-test("Submitting a new task has the correct value in the form", () => {
-    
-    const taskInput = document.querySelector("input");
-    taskInput.value = "Task test";
-
-    const submitButton = document.querySelector("button");
-    // submitButton.click()
-
-    //To do get the data from the user
-    
-
-    
-    const form = document.querySelector("form")
-    
-    equal(taskInput.value, "Task test");
-    taskInput.value = "";
+import { addNewTask } from "../JavaScript/add_new_task.js";
+test("Submitting a new task adds it to the list", () => {
+    // create task
+    let taskInput = document.getElementById("userInput").value;
+    taskInput = "Task test";
+    // submit task
+    addNewTask();
+    equal(taskInput, "Task test");
 })
