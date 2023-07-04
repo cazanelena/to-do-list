3d1bc1453fc74f46012e94e07a9bbc782c8b2ac1
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-07-04 11:32:14 +0100

    when enter is pressed a new task will be sumbitted

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,12 +3,16 @@ import { addNewTask } from "./add_new_task.js";
 import { add_checked } from "./checked_task.js";
 
 
-export let allTasksTracker = [];
 
 const submitBtn = document.getElementById("submitBtn")
 
 submitBtn.addEventListener("click", addNewTask)
 
+document.addEventListener('keydown', function(event) {
+    if(event.key === "Enter") {
+        addNewTask()
+    }
+})
 // Add a checked symbol to a task when clicked
 
 add_checked()
